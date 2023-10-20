dd6b4e1d7546f08f7d212058b1d633f972273515
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-10-20 19:58:40 +0500

    okk

--- src/components/templates/header.js
+++ src/components/templates/header.js
@@ -3,8 +3,11 @@ import Container from './container';
 
 
 
-import moon from '../../assets/images/main-qimg-729a22aba98d1235fdce4883accaf81e.webp'
-import avatar from '../../assets/images/moon4.jpg'
+import moon from '../../assets/images/main-qimg-729a22aba98d1235fdce4883accaf81e.webp';
+import avatar from '../../assets/images/moon4.jpg';
+import russiaflag from '../../assets/images/russiaflad.jpg';
+import ukflag from '../../assets/images/ukflag.webp';
+import uzbflag from '../../assets/images/uzbekflag.webp';
 
 
 import MainBtn from '../ui/MainBtn';
@@ -21,9 +24,9 @@ function Header() {
                 Eng
               </span>
               <ul className={isClicked ? 'lang-modal' : 'lang-modal lang-modalActive' } >
-                 <li>Eng</li>
-                 <li>Rus</li>
-                 <li>Uzb</li>
+                 <li><img className='lang-modal-flag' src={ukflag}/>Eng</li>
+                 <li><img className='lang-modal-flag' src={russiaflag}/>Rus</li>
+                 <li><img className='lang-modal-flag' src={uzbflag}/>Uzb</li>
               </ul>
             </div>
             <div className='header__left'>
