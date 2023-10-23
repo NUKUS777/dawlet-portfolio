dfb4a025f387cdc2d90238bc85f4b2018c592e81
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-10-23 23:23:59 +0500

    ko

--- src/components/templates/footer.js
+++ src/components/templates/footer.js
@@ -30,10 +30,7 @@ function Footer() {
       <h1>GGG</h1>
       <h1>GGG</h1>
       <h1>GGG</h1>
-      <h1>GGG</h1>
-      <h1>GGG</h1>
-      <h1>GGG</h1>
-      <h1>GGG</h1>
+      
     </Container>
   )
 }
