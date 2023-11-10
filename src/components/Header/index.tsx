f17f3233a3f686f24823e9ff39e6943f0a003c4e
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-11-10 15:54:23 +0500

    only with styled components without css or scss

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "../Navbar";
 
 import styled from "styled-components";
+
 interface HeaderProps {
   title: string;
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,10 @@
+import styled from "styled-components";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return <Nav>Navbar</Nav>;
 }
 
+const Nav = styled.div`
+  background: #000;
+`
 export default Navbar;
